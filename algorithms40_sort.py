4a2398fabb773838a1880fd64064d36b8d2ab4fa
NikolayTkachenko <tkachenni@mail.ru>
2024-06-11 01:08:57 +0300

    Update search algoritms

--- algorithms40_sort.py
+++ algorithms40_sort.py
@@ -92,3 +92,80 @@ def ShellSort(list):
     return list
 print("ShellSort")
 print(ShellSort(list01))
+
+#Seletion sort
+list01 = [2, 5, 2, -2, -7, 9, 0, 4]
+
+def SelectionSort(list):
+    for fill_slot in range(len(list) - 1, 0, -1):
+        max_index = 0
+        for location in range(1, fill_slot + 1):
+            if list[location] > list[max_index]:
+                max_index = location
+        list[fill_slot], list[max_index] = list[max_index], list[fill_slot]
+    return list
+
+print("SelectionSort")
+print(SelectionSort(list01))
+
+
+list01 = [2, 5, 2, -2, -7, 9, 0, 4]
+#Linear Search
+def LinearSearch(list, item):
+    index = 0
+    found = False
+
+    while index < len(list) and found is False:
+        if list[index] == item:
+            found = True
+        else:
+            index = index + 1
+    return found
+
+print("Linear Search")
+print(LinearSearch(list01, 9))
+print(LinearSearch(list01, 99))
+
+list01 = [2, 5, 2, -2, -7, 9, 0, 4]
+#BinarySearch
+def BinarySearch(list, item):
+    first = 0
+    last = len(list)-1
+    found = False
+    while first<=last and not found:
+        midpoint = (first + last)//2
+        if list[midpoint] == item:
+            found = True
+        else:
+            if item < list[midpoint]:
+                last = midpoint-1
+            else:
+                first = midpoint+1
+    return found
+
+print("BinarySearch")
+sorted_list = BubbleSort(list01)
+print(BinarySearch(sorted_list, 9))
+print(BinarySearch(sorted_list, 99))
+
+
+list01 = [2, 5, 2, -2, -7, 9, 0, 4]
+#InterpolarySerach
+def IntPolsearch(list, x):
+    idx0 = 0
+    idxn = (len(list)-1)
+    found = False
+
+    while idx0 <= idxn and x >= list[idx0] and x <= list[idxn]:
+        mid = idx0 + int(((float(idxn - idx0)/(list[idxn] - list[idx0])) * (x - list[idx0])))
+        if list[mid] == x:
+            found = True
+            return found
+        if list[mid] < x:
+            idx0 = mid + 1
+    return found
+
+print("InterpolarySerach")
+sorted_list = BubbleSort(list01)
+print(IntPolsearch(sorted_list, 9))
+print(IntPolsearch(sorted_list, 99))
